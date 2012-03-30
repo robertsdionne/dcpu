0fd73ae58b278d0515bc801237d080c0c5ccffec
Robert Dionne <robertsdionne@gmail.com>
2012-03-30 16:55:12 -0400

    Read and write to video memory.

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
 // Copyright 2012 Robert Scott Dionne. All rights reserved.
 
+#include <algorithm>
 #include <ncurses.h>
 #include <string>
 #include "dcpu.h"
@@ -7,11 +8,12 @@
 int main(int argc, char *argv[]) {
   Dcpu dcpu;
   initscr();
-  unsigned short screen[1000];
-  dcpu.ReadVideoMemory(screen);
+  std::string hello_world("Hello, world!");
+  std::copy(hello_world.begin(), hello_world.end(), dcpu.address(0x8000));
   for (int i = 0; i < 1000; ++i) {
-    if (screen[i]) {
-      addch(screen[i]);
+    char character = *dcpu.address(0x8000 + i);
+    if (character) {
+      addch(character);
     }
   }
   refresh();
