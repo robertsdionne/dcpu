98d2ade1a1c3312af45276e5253e23c6d1500a12
Robert Dionne <robertsdionne@gmail.com>
2012-03-31 19:33:41 -0400

    Added tests for all cases of reading from bins and writing to bins.

--- src/dcpu_test.cc
+++ src/dcpu_test.cc
@@ -68,16 +68,155 @@ TEST(DcpuTest, ExecuteCycles) {
   EXPECT_EQ(10, dcpu.program_counter());
 }
 
-TEST(DcpuTest, ExecuteCycle_set_register_with_low_literal) {
+TEST(DcpuTest, ExecuteCycle_set_register_with_register) {
   Dcpu dcpu;
   const Dcpu::Word program[] = {
-    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::k1),
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterB, Dcpu::k1),
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::kRegisterB)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  // dcpu.register_a() == 0 by DcpuTest.DefaultConstructor
+  dcpu.ExecuteCycles(2);
+  EXPECT_EQ(1, dcpu.register_a());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_register_with_location_in_register) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kLocation, Dcpu::k13),
+    0x1000,
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterB, Dcpu::kLiteral),
+    0x1000,
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::kLocationInRegisterB)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycles(3);
+  EXPECT_EQ(13, dcpu.register_a());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_register_with_location_offset_by_register) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kLocation, Dcpu::k13),
+    0x100A,
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterB, Dcpu::k10),
+    Dcpu::Instruct(
+        Dcpu::kSet, Dcpu::kRegisterA, Dcpu::kLocationOffsetByRegisterB),
+    0x1000
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycles(3);
+  EXPECT_EQ(13, dcpu.register_a());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_register_with_pop) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kPush, Dcpu::k13),
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::kPop)
   };
   const Dcpu::Word *const program_end =
       program + sizeof(program)/sizeof(Dcpu::Word);
   std::copy(program, program_end, dcpu.memory_begin());
   dcpu.ExecuteCycle();
-  EXPECT_EQ(1, dcpu.register_a());
+  EXPECT_EQ(0xFFFF, dcpu.stack_pointer());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(0, dcpu.stack_pointer());
+  EXPECT_EQ(13, dcpu.register_a());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_register_with_peek) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kPush, Dcpu::k13),
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::kPeek)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(0xFFFF, dcpu.stack_pointer());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(0xFFFF, dcpu.stack_pointer());
+  EXPECT_EQ(13, dcpu.register_a());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_register_with_push) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::k13),
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::kPush)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(0, dcpu.stack_pointer());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(0xFFFF, dcpu.stack_pointer());
+  EXPECT_EQ(0, dcpu.register_a());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_register_with_stack_pointer) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kPush, Dcpu::k13),
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::kStackPointer)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(0xFFFF, dcpu.stack_pointer());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(0xFFFF, dcpu.register_a());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_register_with_program_counter) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kReserved, Dcpu::k0, Dcpu::k0),
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::kProgramCounter)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycles(2);
+  EXPECT_EQ(2, dcpu.register_a());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_register_with_overflow) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kOverflow, Dcpu::k13),
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::kOverflow)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycles(2);
+  EXPECT_EQ(13, dcpu.register_a());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_register_with_location) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kLocation, Dcpu::k13),
+    0x1000,
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::kLocation),
+    0x1000
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycles(2);
+  EXPECT_EQ(13, dcpu.register_a());
 }
 
 TEST(DcpuTest, ExecuteCycle_set_register_with_high_literal) {
@@ -91,4 +230,157 @@ TEST(DcpuTest, ExecuteCycle_set_register_with_high_literal) {
   std::copy(program, program_end, dcpu.memory_begin());
   dcpu.ExecuteCycle();
   EXPECT_EQ(0x1001, dcpu.register_a());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_register_with_low_literal) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::k1),
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(1, dcpu.register_a());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_location_in_register_with_low_literal) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::kLiteral),
+    0x1000,
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kLocationInRegisterA, Dcpu::k13)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycles(2);
+  EXPECT_EQ(13, *dcpu.address(0x1000));
+}
+
+TEST(DcpuTest, ExecuteCycle_set_location_offset_by_register_with_low_literal) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kRegisterA, Dcpu::k10),
+    Dcpu::Instruct(
+        Dcpu::kSet, Dcpu::kLocationOffsetByRegisterA, Dcpu::k13),
+    0x1000
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycles(2);
+  EXPECT_EQ(13, *dcpu.address(0x100A));
+}
+
+TEST(DcpuTest, ExecuteCycle_set_pop_with_low_literal) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kPush, Dcpu::k13),
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kPop, Dcpu::k14)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycles(2);
+  EXPECT_EQ(14, *dcpu.address(dcpu.stack_pointer() - 1));
+}
+
+TEST(DcpuTest, ExecuteCycle_set_peek_with_low_literal) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kPush, Dcpu::k13),
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kPeek, Dcpu::k14)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycles(2);
+  EXPECT_EQ(14, *dcpu.address(dcpu.stack_pointer()));
+}
+
+TEST(DcpuTest, ExecuteCycle_set_push_with_low_literal) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kPush, Dcpu::k13)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(13, *dcpu.address(dcpu.stack_pointer()));
+}
+
+TEST(DcpuTest, ExecuteCycle_set_stack_pointer_with_low_literal) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kStackPointer, Dcpu::k13)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(13, dcpu.stack_pointer());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_program_counter_with_low_literal) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kProgramCounter, Dcpu::k13)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(13, dcpu.program_counter());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_overflow_with_low_literal) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kOverflow, Dcpu::k13)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(13, dcpu.overflow());
+}
+
+TEST(DcpuTest, ExecuteCycle_set_location_with_low_literal) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kLocation, Dcpu::k13),
+    0x1000
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(13, *dcpu.address(0x1000));
+}
+
+TEST(DcpuTest, ExecuteCycle_set_literal_with_low_literal) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::kLiteral, Dcpu::k13),
+    0x1000
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(0, *dcpu.address(0x1000));
+}
+
+TEST(DcpuTest, ExecuteCycle_set_low_literal_with_low_literal) {
+  Dcpu dcpu;
+  const Dcpu::Word program[] = {
+    Dcpu::Instruct(Dcpu::kSet, Dcpu::k10, Dcpu::k13)
+  };
+  const Dcpu::Word *const program_end =
+      program + sizeof(program)/sizeof(Dcpu::Word);
+  std::copy(program, program_end, dcpu.memory_begin());
+  dcpu.ExecuteCycle();
+  EXPECT_EQ(0, *dcpu.address(0xA));
 }
