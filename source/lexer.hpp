8db4b8cfc88a7d918db200b985669488a5d116bd
Robert Scott Dionne <robertsdionne@gmail.com>
2014-12-08 00:22:30 -0500

    Create a Lexer for DCPU-16 assembly language.

---
+++ source/lexer.hpp
@@ -0,0 +1,91 @@
+#ifndef DCPU_LEXER_HPP_
+#define DCPU_LEXER_HPP_
+
+#include <iostream>
+#include <regex>
+#include <string>
+
+namespace dcpu {
+
+  struct Token {
+    enum class Type {
+      kAdvancedOpcode,
+      kBasicOpcode,
+      kBinary,
+      kColon,
+      kComma,
+      kComment,
+      kData,
+      kDecimal,
+      kHexadecimal,
+      kIdentifier,
+      kInvalid,
+      kLeftBracket,
+      kMinus,
+      kNullptr,
+      kPeek,
+      kPick,
+      kPlus,
+      kPop,
+      kPush,
+      kRightBracket,
+      kSemicolon,
+      kString,
+      kWhitespace
+    };
+    Type type;
+    std::string value;
+  };
+
+  std::ostream &operator <<(std::ostream &out, Token::Type type);
+  std::ostream &operator <<(std::ostream &out, Token token);
+
+  class Lexer {
+  public:
+    static const std::regex kAdvancedOpcodeRegex;
+    static const std::regex kBasicOpcodeRegex;
+    static const std::regex kBinaryRegex;
+    static const std::regex kColonRegex;
+    static const std::regex kCommaRegex;
+    static const std::regex kCommentRegex;
+    static const std::regex kDataRegex;
+    static const std::regex kDecimalRegex;
+    static const std::regex kHexadecimalRegex;
+    static const std::regex kIdentifierRegex;
+    static const std::regex kLeftBracketRegex;
+    static const std::regex kMinusRegex;
+    static const std::regex kNullptrRegex;
+    static const std::regex kPeekRegex;
+    static const std::regex kPickRegex;
+    static const std::regex kPlusRegex;
+    static const std::regex kPopRegex;
+    static const std::regex kPushRegex;
+    static const std::regex kRightBracketRegex;
+    static const std::regex kSemicolonRegex;
+    static const std::regex kStringRegex;
+    static const std::regex kWhitespaceRegex;
+
+  public:
+    Lexer(const std::string &input);
+
+    virtual ~Lexer() = default;
+
+    Token EatToken();
+
+    Token SeeToken(std::string::const_iterator *new_position = nullptr);
+
+  private:
+    void MaybeAdvance(const std::smatch &match, std::string::const_iterator *new_position);
+
+    bool Matches(const std::regex &regex, std::smatch *match);
+
+  private:
+    const std::string &input;
+    std::string::const_iterator position;
+  };
+
+}  // namespace dcpu
+
+
+
+#endif  // DCPU_LEXER_HPP_
