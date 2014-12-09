4145f5eeef42a6ff95044a2986030e0c97b24147
Robert Scott Dionne <robertsdionne@gmail.com>
2014-12-09 05:51:21 -0500

    Clean up namespaces in Parser files.

--- source/parser.cpp
+++ source/parser.cpp
@@ -1,3 +1,5 @@
 #include "parser.hpp"
 
+namespace dcpu {
 
+}  // namespace dcpu
--- source/parser.hpp
+++ source/parser.hpp
@@ -1,9 +1,15 @@
 #ifndef DCPU_PARSER_HPP_
 #define DCPU_PARSER_HPP_
 
-class Parser {
-public:
-  
-};
+namespace dcpu {
+
+  class Parser {
+  public:
+    Parser() = default;
+
+    virtual ~Parser() = default;
+  };
+
+}  // namespace dcpu
 
 #endif  // DCPU_PARSER_HPP_
