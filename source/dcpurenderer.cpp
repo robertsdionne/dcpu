97a352c0814b70e45c82597d7dcb8ec903965b08
Robert Scott Dionne <robertsdionne@gmail.com>
2014-12-23 21:56:39 +0100

    Attempt to call glViewport().

--- source/dcpurenderer.cpp
+++ source/dcpurenderer.cpp
@@ -1,8 +1,13 @@
+#include <GLXW/glxw.h>
+#include <GLFW/glfw3.h>
+
 #include "dcpurenderer.hpp"
 
 namespace dcpu {
-  
-  void DcpuRenderer::Change(int width, int height) {}
+
+  void DcpuRenderer::Change(int width, int height) {
+    glViewport(0, 0, width, height);
+  }
 
   void DcpuRenderer::Create() {}
 
