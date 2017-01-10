dbbe763d5bd76f7b06455214b7bbe5babe86b66e
Robert Scott Dionne <robertsdionne@gmail.com>
2017-01-09 21:57:52 -0500

    Stderr device.

---
+++ stderr/stderr.go
@@ -0,0 +1,72 @@
+package stderr
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+	"log"
+	"os"
+	"unicode/utf16"
+
+	"github.com/robertsdionne/dcpu"
+)
+
+type Stderr struct{}
+
+const (
+	ID             = 0x00000002
+	ManufacturerID = 0x76543210
+	Version        = 0x0000
+)
+
+const (
+	writeWordsAsBytes = iota
+	writeBytes
+	writeUTF16String
+)
+
+func (s *Stderr) Execute(dcpu *dcpu.DCPU) {}
+
+func (s *Stderr) GetID() uint32 {
+	return ID
+}
+
+func (s *Stderr) GetManufacturerID() uint32 {
+	return ManufacturerID
+}
+
+func (s *Stderr) GetVersion() uint16 {
+	return Version
+}
+
+func (s *Stderr) HandleHardwareInterrupt(dcpu *dcpu.DCPU) {
+	length, start := dcpu.RegisterX, dcpu.RegisterY
+	buffer := &bytes.Buffer{}
+	var err error
+
+	switch dcpu.RegisterA {
+	case writeWordsAsBytes:
+		for i := uint16(0); i < length; i++ {
+			err = buffer.WriteByte(byte(dcpu.Memory[start+i]))
+		}
+
+	case writeBytes:
+		for i := uint16(0); i < length; i++ {
+			err = binary.Write(buffer, binary.LittleEndian, dcpu.Memory[start+i])
+		}
+
+	case writeUTF16String:
+		utf16String := string(utf16.Decode(dcpu.Memory[start : start+length]))
+		_, err = buffer.WriteString(utf16String)
+	}
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	bytesWritten, err := io.Copy(os.Stderr, buffer)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	dcpu.RegisterZ = uint16(bytesWritten)
+}
