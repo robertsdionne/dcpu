603736e4b92ac716cf994fc7766a8b6a227c6ba2
Robert Scott Dionne <robertsdionne@gmail.com>
2017-01-31 22:09:15 -0500

    Move dcpu source to file.

--- cmd/dcpu/main.go
+++ cmd/dcpu/main.go
@@ -1,35 +1,25 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
+
 	"github.com/robertsdionne/dcpu"
 	"github.com/robertsdionne/dcpu/clock"
 	"github.com/robertsdionne/dcpu/parser"
 )
 
 func main() {
+	source, err := ioutil.ReadFile("programs/dcpu.dasm")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	cpu := dcpu.DCPU{}
 	clock := clock.Clock{}
 
 	cpu.Hardware = append(cpu.Hardware, &clock)
-
-	cpu.Load(0, parser.Assemble(`
-		:main
-			ias handleInterrupt
-			set a, 0
-			set b, 120
-			hwi 0
-			set a, 2
-			set b, 1
-			hwi 0
-			dum
-			sub pc, 1
-
-		:handleInterrupt
-			dum
-			set a, 1
-			hwi 0
-			rfi 0
-	`))
+	cpu.Load(0, parser.Assemble(string(source)))
 
 	cpu.Execute()
 }
