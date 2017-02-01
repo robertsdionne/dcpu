d678d0b135ddf74e2c952c1993345ad3c1645687
Robert Scott Dionne <robertsdionne@gmail.com>
2017-01-31 21:20:14 -0500

    Port stdinout demo.

--- cmd/stdinout/main.go
+++ cmd/stdinout/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/robertsdionne/dcpu"
+	"github.com/robertsdionne/dcpu/parser"
 	"github.com/robertsdionne/dcpu/stdin"
 	"github.com/robertsdionne/dcpu/stdout"
 )
@@ -13,17 +14,18 @@ func main() {
 
 	cpu.Hardware = append(cpu.Hardware, &in, &out)
 
-	cpu.Load(0, []uint16{
-		dcpu.Basic(dcpu.Set, dcpu.RegisterA, dcpu.Literal0),
-		dcpu.Basic(dcpu.Set, dcpu.RegisterX, dcpu.Literal), 0x0400,
-		dcpu.Basic(dcpu.Set, dcpu.RegisterY, dcpu.Literal), 0x1000,
-		dcpu.Special(dcpu.HardwareInterrupt, dcpu.Literal0),
-		dcpu.Basic(dcpu.Set, dcpu.RegisterA, dcpu.Literal0),
-		dcpu.Basic(dcpu.Set, dcpu.RegisterX, dcpu.RegisterZ),
-		dcpu.Basic(dcpu.Set, dcpu.RegisterY, dcpu.Literal), 0x1000,
-		dcpu.Special(dcpu.HardwareInterrupt, dcpu.Literal1),
-		dcpu.Basic(dcpu.Set, dcpu.ProgramCounter, dcpu.Literal0),
-	})
+	cpu.Load(0, parser.Assemble(`
+		:main
+			set a, 0
+			set x, 0x0400
+			set y, 0x1000
+			hwi 0
+			set a, 0
+			set x, z
+			set y, 0x1000
+			hwi 1
+			set pc, main
+	`))
 
 	cpu.Execute()
 }
