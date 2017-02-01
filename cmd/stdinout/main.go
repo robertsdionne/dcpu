2428f60f6797e92249bd3eacd000cf8ae9e0c4be
Robert Scott Dionne <robertsdionne@gmail.com>
2017-01-31 22:15:26 -0500

    Pull stdinout source into file.

--- cmd/stdinout/main.go
+++ cmd/stdinout/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
+
 	"github.com/robertsdionne/dcpu"
 	"github.com/robertsdionne/dcpu/parser"
 	"github.com/robertsdionne/dcpu/stdin"
@@ -8,24 +11,17 @@ import (
 )
 
 func main() {
+	source, err := ioutil.ReadFile("programs/stdinout.dasm")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	cpu := dcpu.DCPU{}
 	in := stdin.Device{}
 	out := stdout.Device{}
 
 	cpu.Hardware = append(cpu.Hardware, &in, &out)
-
-	cpu.Load(0, parser.Assemble(`
-		:main
-			set a, 0
-			set x, 0x0400
-			set y, 0x1000
-			hwi 0
-			set a, 0
-			set x, z
-			set y, 0x1000
-			hwi 1
-			set pc, main
-	`))
+	cpu.Load(0, parser.Assemble(string(source)))
 
 	cpu.Execute()
 }
