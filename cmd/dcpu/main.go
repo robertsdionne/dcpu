aad7d5f5a8f42183b040eb7f3272e02866688478
Robert Scott Dionne <robertsdionne@gmail.com>
2024-06-01 09:18:03 -0700

    Consolidate commands.

--- cmd/dcpu/main.go
+++ cmd/dcpu/main.go
@@ -1,24 +1,70 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/robertsdionne/dcpu"
 	"github.com/robertsdionne/dcpu/assembler"
 	"github.com/robertsdionne/dcpu/clock"
+	"github.com/robertsdionne/dcpu/floppy"
+	"github.com/robertsdionne/dcpu/hardware"
+	"github.com/robertsdionne/dcpu/keyboard"
+	"github.com/robertsdionne/dcpu/monitor"
+	"github.com/robertsdionne/dcpu/sped3"
+	"github.com/robertsdionne/dcpu/stderr"
+	"github.com/robertsdionne/dcpu/stdin"
+	"github.com/robertsdionne/dcpu/stdout"
+)
+
+var (
+	floppyDisk = flag.String("floppy-disk", "", "The floppy file.")
+	program    = flag.String("program", "/dev/stdin", "Which program to run.")
+	useSPED3   = flag.Bool("sped3", false, "Use SPED3 instead of LEM1802")
 )
 
 func main() {
-	program, err := assembler.AssembleFile("programs/dcpu.dasm")
+	flag.Parse()
+
+	program, err := assembler.AssembleFile(*program)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	cpu := dcpu.DCPU{}
-	clock := clock.Clock{}
+	k := keyboard.Device{}
+	m := monitor.Device{
+		VideoAddress: 0x8000,
+	}
+	s := sped3.Device{TargetRotation: 90}
+	c := clock.Clock{}
+	i := stdin.Device{}
+	o := stdout.Device{}
+	e := stderr.Device{}
+	f := floppy.Device{}
 
-	cpu.Hardware = append(cpu.Hardware, &clock)
+	if *useSPED3 {
+		cpu.Hardware = append(cpu.Hardware, &s)
+	} else {
+		cpu.Hardware = append(cpu.Hardware, &m)
+	}
+	cpu.Hardware = append(cpu.Hardware, &k, &c, &i, &o, &e, &f)
+
+	k.Init()
+	if *floppyDisk != "" {
+		f.Insert(*floppyDisk, false)
+	}
 	cpu.Load(0, program)
 
-	cpu.Execute()
+	go cpu.Execute()
+
+	loop := hardware.Loop{
+		Keyboard: &k,
+	}
+	if *useSPED3 {
+		loop.SPED3 = &s
+	} else {
+		loop.Monitor = &m
+	}
+	loop.Run()
 }
