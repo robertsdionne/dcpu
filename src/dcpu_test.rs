12b4fbbe109b1e70cc4fe58cf7d5ec8cf30f5370
Robert Scott Dionne <robertsdionne@gmail.com>
2025-01-11 08:26:50 -0800

    Add unit tests for add, subtract, multiply.

--- src/dcpu_test.rs
+++ src/dcpu_test.rs
@@ -2,6 +2,90 @@ use crate::dcpu::*;
 use crate::instructions::{BasicOpcode, Instruction, OperandA, OperandB};
 
 #[test]
+fn add_small_literal_to_register() {
+    let mut cpu = Dcpu::default();
+    let mut hardware = vec![];
+    cpu.load(0, &vec![
+        Instruction::Basic(BasicOpcode::Set, OperandB::RegisterA, OperandA::SmallLiteral(13)).into(),
+        Instruction::Basic(BasicOpcode::Add, OperandB::RegisterA, OperandA::SmallLiteral(14)).into(),
+    ]);
+
+    cpu.execute_instructions(&mut hardware, 2);
+    assert_eq!(0x1b, cpu.register_a);
+    assert_eq!(0, cpu.extra);
+}
+
+#[test]
+fn add_register_with_overflow() {
+    let mut cpu = Dcpu::default();
+    let mut hardware = vec![];
+    cpu.load(0, &vec![
+        Instruction::Basic(BasicOpcode::Set, OperandB::RegisterA, OperandA::SmallLiteral(-1)).into(),
+        Instruction::Basic(BasicOpcode::Add, OperandB::RegisterA, OperandA::SmallLiteral(-1)).into(),
+    ]);
+
+    cpu.execute_instructions(&mut hardware, 2);
+    assert_eq!(0xfffe, cpu.register_a);
+    assert_eq!(1, cpu.extra);
+}
+
+#[test]
+fn subtract_small_literal_from_register() {
+    let mut cpu = Dcpu::default();
+    let mut hardware = vec![];
+    cpu.load(0, &vec![
+        Instruction::Basic(BasicOpcode::Set, OperandB::RegisterA, OperandA::SmallLiteral(30)).into(),
+        Instruction::Basic(BasicOpcode::Subtract, OperandB::RegisterA, OperandA::SmallLiteral(16)).into(),
+    ]);
+
+    cpu.execute_instructions(&mut hardware, 2);
+    assert_eq!(0xe, cpu.register_a);
+    assert_eq!(0, cpu.extra);
+}
+
+#[test]
+fn subtract_register_with_underflow() {
+    let mut cpu = Dcpu::default();
+    let mut hardware = vec![];
+    cpu.load(0, &vec![
+        Instruction::Basic(BasicOpcode::Set, OperandB::RegisterA, OperandA::SmallLiteral(16)).into(),
+        Instruction::Basic(BasicOpcode::Subtract, OperandB::RegisterA, OperandA::SmallLiteral(30)).into(),
+    ]);
+
+    cpu.execute_instructions(&mut hardware, 2);
+    assert_eq!(0xfff2, cpu.register_a);
+    assert_eq!(0xffff, cpu.extra);
+}
+
+#[test]
+fn multiply_small_literal_with_register() {
+    let mut cpu = Dcpu::default();
+    let mut hardware = vec![];
+    cpu.load(0, &vec![
+        Instruction::Basic(BasicOpcode::Set, OperandB::RegisterA, OperandA::SmallLiteral(16)).into(),
+        Instruction::Basic(BasicOpcode::Multiply, OperandB::RegisterA, OperandA::SmallLiteral(30)).into(),
+    ]);
+
+    cpu.execute_instructions(&mut hardware, 2);
+    assert_eq!(0x1e0, cpu.register_a);
+    assert_eq!(0, cpu.extra);
+}
+
+#[test]
+fn multiply_multiply_register_with_overflow() {
+    let mut cpu = Dcpu::default();
+    let mut hardware = vec![];
+    cpu.load(0, &vec![
+        Instruction::Basic(BasicOpcode::Set, OperandB::RegisterA, OperandA::SmallLiteral(-1)).into(),
+        Instruction::Basic(BasicOpcode::Multiply, OperandB::RegisterA, OperandA::SmallLiteral(-1)).into(),
+    ]);
+
+    cpu.execute_instructions(&mut hardware, 2);
+    assert_eq!(0x1, cpu.register_a);
+    assert_eq!(0xfffe, cpu.extra);
+}
+
+#[test]
 fn if_above_with_lesser_small_literal() {
     let mut cpu = Dcpu::default();
     let mut hardware = vec![];
