87a766393c9fd28077eaa1a7ae433accbc0cf3b0
Robert Scott Dionne <robertsdionne@gmail.com>
2025-01-16 00:14:40 -0800

    Haskell from and into (partial).

--- src/instructions.hs
+++ src/instructions.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.Bits
+
 main :: IO [()]
 main = mapM print [
         Special InterruptAddressSet (LeftValue (WithPayload Literal 11)),
@@ -35,11 +37,61 @@ main = mapM print [
 class Sizeable a where
     size :: a -> Int
 
+class From a b where
+    from :: a -> b
+
+class Into a b where
+    into :: b -> a
+
+newtype InstructionValue = InstructionValue Int
+
+instance From Int InstructionValue where
+    from value = InstructionValue value
+
+instance Into Int InstructionValue where
+    into (InstructionValue value) = value
+
 data Instruction =
     Basic BasicOpcode OperandB OperandA
   | Special SpecialOpcode OperandA
   | Debug DebugOpcode
     deriving (Show)
+
+instance From Int Instruction where
+    from value =
+        if basic_opcode /= Reserved then
+            let operand_a :: OperandA = from instruction
+                operand_b :: OperandB = from instruction
+            in Basic basic_opcode operand_b operand_a
+        else if special_opcode /= SpecialReserved then
+            let operand_a :: OperandA = from instruction
+            in Special special_opcode operand_a
+        else
+            Debug debug_opcode
+        where instruction :: InstructionValue = from value
+              basic_opcode :: BasicOpcode = from instruction
+              special_opcode :: SpecialOpcode = from instruction
+              debug_opcode :: DebugOpcode = from instruction
+
+instance Into Int Instruction where
+    into (Basic basic_opcode operand_b operand_a) =
+        let basic_opcode_value :: InstructionValue = into basic_opcode
+            operand_a_value :: InstructionValue = into operand_a
+            operand_b_value :: InstructionValue = into operand_b
+        in let basic_opcode :: Int = into basic_opcode_value
+               operand_a :: Int = into operand_a_value
+               operand_b :: Int = into operand_b_value
+        in basic_opcode .|. operand_b .|. operand_a
+    into (Special special_opcode operand_a) =
+        let special_opcode_value :: InstructionValue = into special_opcode
+            operand_a_value :: InstructionValue = into operand_a
+        in let special_opcode :: Int = into special_opcode_value
+               operand_a :: Int = into operand_a_value
+        in special_opcode .|. operand_a
+    into (Debug debug_opcode) =
+        let debug_opcode_value :: InstructionValue = into debug_opcode
+        in let debug_opcode :: Int = into debug_opcode_value
+        in debug_opcode
 
 instance Sizeable Instruction where
     size (Basic _ operand_b operand_a) = 1 + size operand_b + size operand_a
@@ -79,7 +131,13 @@ data BasicOpcode =
   | Unused1d
   | SetThenIncrement
   | SetThenDecrement
-    deriving (Enum, Show)
+    deriving (Enum, Eq, Show)
+
+instance From InstructionValue BasicOpcode where
+    from value = Reserved
+
+instance Into InstructionValue BasicOpcode where
+    into basic_opcode = InstructionValue (fromEnum basic_opcode)
 
 data SpecialOpcode =
     SpecialReserved
@@ -100,18 +158,36 @@ data SpecialOpcode =
   | HardwareNumberConnected
   | HardwareQuery
   | HardwareInterrupt
-    deriving (Enum, Show)
+    deriving (Enum, Eq, Show)
+
+instance From InstructionValue SpecialOpcode where
+    from value = SpecialReserved
+
+instance Into InstructionValue SpecialOpcode where
+    into special_opcode = InstructionValue (fromEnum special_opcode)
 
 data DebugOpcode =
     Noop
   | Alert
   | DumpState
-    deriving (Enum, Show)
+    deriving (Enum, Eq, Show)
+
+instance From InstructionValue DebugOpcode where
+    from value = Alert
+
+instance Into InstructionValue DebugOpcode where
+    into debug_opcode = InstructionValue (fromEnum debug_opcode)
 
 data OperandA =
     LeftValue OperandB
   | SmallLiteral Int
     deriving (Show)
+
+instance From InstructionValue OperandA where
+    from value = SmallLiteral 3
+
+instance Into InstructionValue OperandA where
+    into basic_opcode = InstructionValue 2
 
 instance Sizeable OperandA where
     size (LeftValue operand_b) = size operand_b
@@ -126,6 +202,12 @@ data OperandB =
   | ProgramCounter
   | Extra
     deriving (Show)
+
+instance From InstructionValue OperandB where
+    from value = WithRegister Register RegisterA
+
+instance Into InstructionValue OperandB where
+    into basic_opcode = InstructionValue 1
 
 instance Sizeable OperandB where
     size (WithPayload _ _) = 1
